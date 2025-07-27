bbe403c8203895fe9f2d35b5526ad3836e9c7b4a
José Sánchez <sanchedev@outlook.com>
2025-07-27 17:18:33 -0500

    Agregando un sistema de comida y salud

---
+++ scenes/player/player.gd
@@ -0,0 +1,51 @@
+class_name Player
+extends CharacterBody3D
+
+@export var speed = 4
+@export var run_speed = 7
+@export var mouse_sensitivity = 0.004
+
+@onready var camera = $Camera3D
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+var is_running = false
+
+# Movement
+func _ready() -> void:
+	add_to_group("player")
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	instance = self
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	
+	is_running = Input.is_action_pressed("run") and HealthController.food_bar >= HealthController.full_bar/2
+	
+	var current_speed = speed
+	if is_running: current_speed = run_speed
+	
+	var input_dir = Input.get_vector("left", "right", "forward", "back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
+
+	if input_dir == Vector2.ZERO:
+		HealthController.use_food_to_run()
+	
+	move_and_slide()
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		camera.rotation.x -= event.relative.y * mouse_sensitivity
+		rotation.y -= event.relative.x * mouse_sensitivity
+		
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-45), deg_to_rad(60))
+
+static var instance: Player
---
+++ scripts/controllers/health_controller.gd
@@ -0,0 +1,12 @@
+class_name HealthController
+
+static var food_bar = 100
+static var health_bar = 100
+
+static var full_bar = 100
+
+static func use_food_to_run():
+	pass
+
+static func use_food_to_regenerate():
+	pass
